1eeb6c40721a7d3ba9db800c7a7ce60312665887
VitusLam <99128159+VitusLam@users.noreply.github.com>
2025-03-03 03:51:00 +0000

    add exercise

---
+++ exercise.test.js
@@ -0,0 +1,7 @@
+import { reverseString } from './exercise.js';
+
+test('reverseString reverses the string', () => {
+    expect(reverseString('hello')).toBe('olleh');
+    // add more tests here
+    expect(reverseString('world)')).toBe(')dlrow');
+});
